930dd83f041032c9bc3dd2f9d2050f0071aad4af
Jacob Lange <jacoblange007@yahoo.com>
2022-07-08 16:01:34 -0500

    added constructor and instance variables

--- LiquorWine.java
+++ LiquorWine.java
@@ -3,6 +3,22 @@ public class LiquorWine {
 
   //age
   
+  private String name;
+  private double unitCost;
+  private int quantity;
+  private int rating;
+
+  public LiquorWine(String name, double unitCost, int quantity, int rating){
+    this.name = name;
+    this.unitCost = unitCost;
+    this.quantity = quantity;
+    this.rating = rating;
+  }
+
+
+  
+
+
   
   
 }
