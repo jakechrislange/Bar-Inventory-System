6db9c32861fb09cdb50f9204e73769307577c7a7
jacoblange007@yahoo.com <jacoblange007@yahoo.com>
2022-08-20 09:54:40 -0500

    starts building shelf BST class

--- Shelf.java
+++ Shelf.java
@@ -2,4 +2,50 @@
 
 public class Shelf {
 
+    private BSTNode<LiquorWine> root; // root node of shelf BST
+    private int size; // size of the self
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean findDrink(String name, double unitCost, int quantity, int rating){
+       LiquorWine drink = new LiquorWine(name, unitCost, quantity, rating);
+        
+        if (isEmpty()){
+            return false;
+        }
+
+        return findDrinkHelper(drink, root);      
+    }
+
+    protected static boolean findDrinkHelper(LiquorWine targetDrink, BSTNode<LiquorWine> curr){
+
+        if (curr == null){
+            return false;
+        }
+
+        if (targetDrink.equals(curr.getData())){
+            return true;
+        }
+
+        int comparison = curr.getData().compareTo(targetDrink);
+
+        if (comparison < 0){
+            return findDrinkHelper(targetDrink, curr.getRight());
+        }else{
+            return findDrinkHelper(targetDrink, curr.getLeft());
+        }
+
+    }
+
+
+
+
+
+
 }
